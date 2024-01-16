48f79bfae5a10480f5a98800bd3f0f77c81f7ccb
sakshi jadhav <sakshisjadhav.120@gmail.com>
2024-01-17 00:04:04 +0530

    upadted

---
+++ word.js
@@ -0,0 +1,15 @@
+// const arr = "word"
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i);
+// }
+
+const arr = "world"
+const arr1 = ""
+
+for(i = 0; i< arr.length; i++ ){
+  console.log(i)
+  arr1.push(arr[i])
+} 
+// return arr1
+console.log(arr1);
